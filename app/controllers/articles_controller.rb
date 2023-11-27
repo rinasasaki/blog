757e8063b76f3d0ccfa2d314fd54acdcb67b22cd
rinasasaki <1072sasaki@gmail.com>
2023-11-27 20:55:08 +0900

    [追加]削除ボタンを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   def edit
     @article = Article.find(params[:id])
   end
@@ -36,6 +36,13 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+  
   private
   def article_params
     params.require(:article).permit(:title, :body)
