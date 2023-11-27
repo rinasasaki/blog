7f6b390f9a359ec4856a968aa0dd864419a53f49
rinasasaki <1072sasaki@gmail.com>
2023-11-27 23:03:36 +0900

    英語化対応

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,8 @@ module Blog
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
